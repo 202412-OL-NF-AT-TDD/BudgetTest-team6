adc2199bfc34480b5ec00c966eb5a1c91617596d
joeychen <joeychen@odd-e.com>
2024-12-25 22:12:14 +0800

    inline variable: budgets

--- BudgetTest/BudgetTest/BudgetService.cs
+++ BudgetTest/BudgetTest/BudgetService.cs
@@ -16,10 +16,8 @@ public class BudgetService
             return 0m;
         }
 
-        var budgets = _budgetRepo.GetAll();
-
         var period = new Period(start, end);
 
-        return budgets.Sum(budget => budget.OverlappingAmount(period));
+        return _budgetRepo.GetAll().Sum(budget => budget.OverlappingAmount(period));
     }
 }
