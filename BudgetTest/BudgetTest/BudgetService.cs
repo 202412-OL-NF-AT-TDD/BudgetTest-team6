58afcdb7b1f7eba41510f72340651aca740f5863
joeychen <joeychen@odd-e.com>
2024-12-25 22:12:04 +0800

    remove redundant code block

--- BudgetTest/BudgetTest/BudgetService.cs
+++ BudgetTest/BudgetTest/BudgetService.cs
@@ -18,24 +18,6 @@ public class BudgetService
 
         var budgets = _budgetRepo.GetAll();
 
-        if (start.ToString("yyyyMM") == end.ToString("yyyyMM"))
-        {
-            var yearMonth = start.ToString("yyyyMM");
-            var budget = budgets.FirstOrDefault(budget => budget.YearMonth == yearMonth);
-
-            if (budget == null)
-            {
-                return 0m;
-            }
-
-            var amount = budget.Amount;
-            var daysInMonth = DateTime.DaysInMonth(start.Year, start.Month);
-            var days = (end - start).Days + 1;
-            var result = amount / daysInMonth * days;
-
-            return result;
-        }
-
         var period = new Period(start, end);
 
         return budgets.Sum(budget => budget.OverlappingAmount(period));
