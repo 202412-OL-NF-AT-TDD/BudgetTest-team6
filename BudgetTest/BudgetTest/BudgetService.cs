fc94f84edb20a7aa11dccbfa2dc51f0df1a6d272
joeychen <joeychen@odd-e.com>
2024-12-25 22:01:37 +0800

    introduce variables: overlappingEnd and overlappingStart

--- BudgetTest/BudgetTest/BudgetService.cs
+++ BudgetTest/BudgetTest/BudgetService.cs
@@ -47,18 +47,21 @@ public class BudgetService
             int overlappingDays;
             if (budget.YearMonth == start.ToString("yyyyMM"))
             {
-                overlappingDays = (budget.LastDay() - start).Days + 1;
-                // totalBudget += budget.DailyAmount() * overlappingDays;
+                var overlappingEnd = budget.LastDay();
+                var overlappingStart = start;
+                overlappingDays = (overlappingEnd - overlappingStart).Days + 1;
             }
             else if (budget.YearMonth == end.ToString("yyyyMM"))
             {
-                overlappingDays = (end - budget.FirstDay()).Days + 1;
-                // totalBudget += budget.DailyAmount() * overlappingDays;
+                var overlappingEnd = end;
+                var overlappingStart = budget.FirstDay();
+                overlappingDays = (overlappingEnd - overlappingStart).Days + 1;
             }
             else
             {
-                overlappingDays = (budget.LastDay() - budget.FirstDay()).Days + 1;
-                // totalBudget += budget.DailyAmount() * overlappingDays;
+                var overlappingEnd = budget.LastDay();
+                var overlappingStart = budget.FirstDay();
+                overlappingDays = (overlappingEnd - overlappingStart).Days + 1;
             }
 
             totalBudget += budget.DailyAmount() * overlappingDays;
