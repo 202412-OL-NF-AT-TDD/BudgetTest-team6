386b486a9d46bd9fff153b8222bba10ab177bbee
joeychen <joeychen@odd-e.com>
2024-12-25 22:06:25 +0800

    clean up

--- BudgetTest/BudgetTest/Period.cs
+++ BudgetTest/BudgetTest/Period.cs
@@ -19,29 +19,13 @@ public class Period
             return 0;
         }
 
-        DateTime overlappingEnd = End < budget.LastDay()
+        var overlappingEnd = End < budget.LastDay()
             ? End
             : budget.LastDay();
-        DateTime overlappingStart = Start > budget.FirstDay()
+        var overlappingStart = Start > budget.FirstDay()
             ? Start
             : budget.FirstDay();
-        if (budget.YearMonth == Start.ToString("yyyyMM"))
-        {
-            // overlappingEnd = budget.LastDay();
-            // overlappingStart = Start;
-        }
-        else if (budget.YearMonth == End.ToString("yyyyMM"))
-        {
-            // overlappingEnd = End;
-            // overlappingStart = budget.FirstDay();
-        }
-        else
-        {
-            // overlappingEnd = budget.LastDay();
-            // overlappingStart = budget.FirstDay();
-        }
 
-        var overlappingDays = (overlappingEnd - overlappingStart).Days + 1;
-        return overlappingDays;
+        return (overlappingEnd - overlappingStart).Days + 1;
     }
 }
